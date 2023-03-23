23c9e2372afa2254767eea719dc8a6130eb1d41b
sjsidor <95105246+sjsidor@users.noreply.github.com>
2023-03-23 09:13:57 -0400

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def hello() -> str:
     # https://cloud.google.com/run/docs/logging#correlate-logs
     logger.info("Child logger with trace Id.")
 
-    return "Hey it's Stephen! I set up a containter on GCP & deployed a CI/CD pipeline!"
+    return "Steph is really good at wordle and the crossword, what do you think?"
 
 
 def shutdown_handler(signal_int: int, frame: FrameType) -> None:
