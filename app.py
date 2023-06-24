463a3a9e32c1ee2eb5f9104cf16db4f090134c67
sjsidor <95105246+sjsidor@users.noreply.github.com>
2023-06-24 12:19:02 -0400

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def hello() -> str:
     # https://cloud.google.com/run/docs/logging#correlate-logs
     logger.info("Child logger with trace Id.")
 
-    return "Lean Cofffee with Maddie, Emma, and Kendall."
+    return "I love Dani VC"
 
 
 def shutdown_handler(signal_int: int, frame: FrameType) -> None:
